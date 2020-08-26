cc9f270059454bab157261c984fc992d22872f62
DSusan <carlissian1996@hotmail.com>
2020-08-26 18:32:55 -0500

    added app name in urls

--- apps/documento/urls.py
+++ apps/documento/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from .views import index_doc, index_his, index_vin
-
+app_name = 'documento'
 urlpatterns = [
     url(r'^$', index_doc, name='index'),
     url(r'^historia$', index_his, name='index_his'),
